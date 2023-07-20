bd175c42919f999d5bfdc0352369ac23234c235a
Salvatore <salvodanna@yahoo.it>
2023-07-20 15:51:20 +0200

    Esercizio kotlin Inheritance and composition AGGIORNATO

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,20 @@
-
-
 fun main(args: Array<String>) {
+    //INTERFACES
     val n1 = Sum(43, 7)
     val n2 = Subtraction(50, 25)
-    //println(n1.operation())
-   //println(n2.operation())
-    val cucciolo = Pet("cane", 6)
+    println(n1.operation())
+    println(n2.operation())
+
+    //INHERITANCE
+    val cucciolo = Pet("Cane", 6)
     val cucciolo1 = Fido(7)
     val cucciolo2 = Salem(3)
     println(cucciolo.showDetails())
     println(cucciolo1.showDetails())
     println(cucciolo2.showDetails())
-
 }
 
-
+// INTERFACES:
 interface Calculator {
     val num1: Int
     val num2: Int
@@ -33,20 +33,26 @@ class Subtraction(override val num1: Int, override val num2: Int) : Calculator {
     }
 }
 
-open class Pet(val type: String,open val age: Int) {
+
+//INHERITANCE
+
+
+open class Pet(val type: String, open val age: Int) {
     open fun showDetails(): String {
-        return "type of pet : $type age of pet : $age"
+        return "Type of pet : $type  Age of pet : $age"
     }
 }
-class Fido(override val age: Int):Pet("Cane",age){
+
+class Fido(override val age: Int) : Pet("Cane", age) {
     override fun showDetails(): String {
-        return "Pet details are : " + super.showDetails()
+        return  super.showDetails()
     }
 
 }
-class Salem(override val age: Int):Pet("Gatto",age){
+
+class Salem(override val age: Int) : Pet("Gatto", age) {
     override fun showDetails(): String {
-        return "Pet details are : " + super.showDetails()
+        return super.showDetails()
     }
 
 }
